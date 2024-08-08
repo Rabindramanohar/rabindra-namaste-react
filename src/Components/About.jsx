6dbff6ead04c54d09e22287bafabc7be91ee872d
rabindra <robinmanohar@gmail.com>
2024-08-08 16:59:55 +0530

    update with aboutus class component and notes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,7 +1,11 @@
+// import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
   return (
     <div className="about-container">
       <h1>About Us</h1>
+      <UserClass name={"rabindra"} handle={"rabindramanohar"} />
     </div>
   );
 };
